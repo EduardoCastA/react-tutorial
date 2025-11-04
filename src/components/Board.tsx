c6f8d01677a40cf0b6fa5b242495e8901f78ff80
EduardoCastA <a311946@alumnos.uaslp.mx>
2025-11-04 17:54:14 -0600

    Agregar texto indicando el turno y quien es el ganador

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,10 +4,10 @@ import { Square } from "./Square";
 
 export const Board = () => {
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null));
 
   function handleClick(index: number) {
-    if (squares[index]) return;
+    if (squares[index] || calculateWinner(squares)) return;
 
     const nextSquares = squares.slice();
     nextSquares[index] = xIsNext ? "X" : "O";
@@ -16,8 +16,19 @@ export const Board = () => {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  const isDraw = !winner && !squares.some(value => value === null);
+
+  let status;
+  if (winner) {
+    status = 'Ganador: ' + winner;
+  } else {
+    status = isDraw ? 'Juego terminado (Empate)' : 'Siguiente jugador: ' + (xIsNext ? 'X' : 'O');
+  }
+
   return (
     <>
+      <div className="status">{ status }</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -35,4 +46,24 @@ export const Board = () => {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares: Array<(string | null)>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
