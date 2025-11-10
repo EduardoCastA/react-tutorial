2f8d05629ac43f7fce4e71d990ce293f70b3161e
EduardoCastA <a311946@alumnos.uaslp.mx>
2025-11-10 13:19:36 -0600

    Cambios en el componente Board para usar bucles al crear el tablero

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import { Square } from "./Square";
 
 interface Props {
@@ -7,6 +5,9 @@ interface Props {
   squares: Array<string | null>;
   onPlay: (nextSquares: Array<string | null>) => void;
 }
+
+const rows = [0, 1, 2];
+const cols = [0, 1, 2];
 
 export const Board = ({ xIsNext, squares, onPlay }: Props) => {
   function handleClick(index: number) {
@@ -31,21 +32,20 @@ export const Board = ({ xIsNext, squares, onPlay }: Props) => {
   return (
     <>
       <div className="status">{ status }</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {rows.map((row) => (
+        <div key={row} className="board-row">
+          {cols.map((col) => {
+            const index = row * 3 + col;
+            return (
+              <Square
+                key={index}
+                value={squares[index]}
+                onSquareClick={() => handleClick(index)}
+              />
+            );
+          })}
+        </div>
+      ))}
     </>
   );
 }
